7a4d96dec946a16ac19d4b4c2586482f64bb6e9d
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-11-09 22:21:52 +0530

    API setup

---
+++ app.js
@@ -0,0 +1,13 @@
+const input  = document.querySelector('input')
+const btn = document.getElementById("btn");
+const icon  = document.querySelector(".icon")
+const temperature  = document.querySelector(".temperature")
+const description  = document.querySelector(".description")
+
+btn.addEventListener('clicked',()=>{
+    console.log("clicked")
+})
+
+
+
+
